0919474874a8aaf376eeacb7c3be92aa41ac4661
SergeHall <sergioartg@gmail.com>
2022-03-22 20:20:33 -0700

    06 kyu kata

---
+++ 06_kyu_Create_Phone_Number.py
@@ -0,0 +1,29 @@
+# Write a function that accepts an array of 10 integers (between 0 and 9),
+# that returns a string of those numbers in the form of a phone number.
+# Example:
+# createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(
+# 123) 456-7890"
+# The returned format must be correct in order to complete this challenge.
+# Don't forget the space after the closing parentheses!
+
+
+def create_phone_number(mylist):
+    code = mylist[:3]
+    numbers3 = mylist[3:6]
+    numbers4 = mylist[6:10]
+    phone_number = "("
+
+    for i in code:
+        phone_number += str(i)
+
+    phone_number += ") "
+
+    for i in numbers3:
+        phone_number += str(i)
+
+    phone_number += "-"
+
+    for i in numbers4:
+        phone_number += str(i)
+
+    return phone_number
